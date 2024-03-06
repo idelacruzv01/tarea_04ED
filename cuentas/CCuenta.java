35589f6c63210acd259f7d7d009324baf3538e05
Ivan <idelacruzvelasco@gmail.com>
2024-03-06 12:16:46 +0100

    Encapsulo atributos clase CCuenta y quito tilde a tipoInteres

--- cuentas/CCuenta.java
+++ cuentas/CCuenta.java
@@ -5,7 +5,7 @@ public class CCuenta {
     private String nombre;
     private String cuenta;
     private double saldo;
-    private double tipoInterés;
+    private double tipoInteres;
 
     public CCuenta()
     {
@@ -13,21 +13,21 @@ public class CCuenta {
 
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+        setNombre(nom);
+        setCuenta(cue);
+        setSaldo(sal);
     }
 
     public double estado()
     {
-        return saldo;
+        return getSaldo();
     }
 
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);
     }
 
     public void retirar(double cantidad) throws Exception
@@ -36,6 +36,38 @@ public class CCuenta {
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        setSaldo(getSaldo() - cantidad);
     }
+
+	protected String getNombre() {
+		return nombre;
+	}
+
+	protected void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	protected String getCuenta() {
+		return cuenta;
+	}
+
+	protected void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	protected double getSaldo() {
+		return saldo;
+	}
+
+	protected void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	protected double getTipoInterés() {
+		return tipoInteres;
+	}
+
+	protected void setTipoInterés(double tipoInterés) {
+		this.tipoInteres = tipoInterés;
+	}
 }
